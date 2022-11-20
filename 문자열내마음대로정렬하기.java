117ecadcadebf94c775206205a7ff138faac4e59
Xogo8565 <xogo8565@gmail.com>
2022-11-20 20:46:36 +0900

    Create 문자열내마음대로정렬하기.java

---
+++ 문자열내마음대로정렬하기.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] temp = new String[strings.length];
+        for(int i = 0; i<strings.length; i++){
+            temp[i] = strings[i].charAt(n) + strings[i];
+        }
+        Arrays.sort(temp);
+        for(int i = 0; i<strings.length; i++){
+            temp[i] = temp[i].substring(1);
+        }
+        return temp;
+    }
+}
