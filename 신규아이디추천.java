57ad5282d509276812eac7c92a53ab6f6e4e3083
Xogo8565 <xogo8565@gmail.com>
2023-01-08 23:30:37 +0900

    Create 신규아이디추천.java

---
+++ 신규아이디추천.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase(); // 1
+
+        answer = answer.replaceAll("[^-_.a-z0-9]", ""); // 2
+        answer = answer.replaceAll("[.]{2,}", "."); // 3
+        answer = answer.replaceAll("^[.]|[.]$", "");    // 4
+        
+        if (answer.equals("")) {    // 5
+            answer += "a";
+        }
+
+        if (answer.length() >= 16) {     // 6
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$","");
+        }
+
+        if (answer.length() <= 2) {  // 7
+            while (answer.length() < 3) {
+                answer += answer.charAt(answer.length()-1);
+            }
+        }
+
+        return answer;
+    }
+}
